e003ef3bf0731d8fb76e4afc9e30933606b865cc
Shan AZIZ <shan.azizh@gmail.com>
2022-03-02 13:25:33 +0100

    build : domains

---
+++ src/main/java/fr/esgi/restapi/domain/Group.java
@@ -0,0 +1,20 @@
+package fr.esgi.restapi.domain;
+
+import java.util.List;
+
+public class Group {
+
+    private List<Student> students;
+
+    public Group(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
